65926deb2febacbba08839a4c5d438add6563984
Al-Amin <116742755+adnanalamin@users.noreply.github.com>
2024-09-02 22:51:46 +0600

    Routing, respons, status code

--- day1/index.js
+++ day1/index.js
@@ -8,6 +8,51 @@ app.get('/', (req, res) => {
     res.send('Helle express js')
 })
 
+// Simple string respons
+app.get('/stringRespons', (req,res) => {
+    res.send("This is simple string response")
+})
+app.post('/stringRespons2', (req,res) => {
+    res.send("This is simple string response")
+})
+
+// Status code respons
+app.get('/stringRespons3', (req,res) => {
+    res.status(401).end("Unathorize")
+})
+
+
+// Json Respons
+app.get('/jsonresponse', (req,res) => {
+
+    const myJsonFile=[
+        {
+            name:'Adnan',
+            city:'Khulna',
+            occupation:'Engr.'
+        },
+        {
+            name:'Al Amin',
+            city:'Khulna',
+            occupation:'Programmer'
+        },
+        {
+            name:'Rubel',
+            city:'Khulna',
+            occupation:'Bus'
+        },
+        {
+            name:'Adnan',
+            city:'Khulna',
+            occupation:'Engr.'
+        },
+    ]
+    res.json(myJsonFile)
+})
+
+
+
+
 
 
 app.listen(port, () => {

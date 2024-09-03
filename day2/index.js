342b2ae43635c4d11be12b481d4864697bdcbe95
Al-Amin <116742755+adnanalamin@users.noreply.github.com>
2024-09-03 11:54:58 +0600

    Learn about redirect responce

--- day2/index.js
+++ day2/index.js
@@ -2,9 +2,20 @@ const express = require('express');
 const app = express();
 const port = 5000;
 
+// Download response
 app.get('/dwonloadResponse', (req, res) => {
     res.download('./uploads/abc.jpg');
 })
+
+// Redirect response
+app.get('/bangladesh', (req, res) => {
+    res.send("I Love Bangladesh")
+})
+
+app.get('/india', (req, res) => {
+    res.redirect('http://localhost:5000/bangladesh')
+})
+
 
 app.listen(port, () => {
     console.log("Server is running");
